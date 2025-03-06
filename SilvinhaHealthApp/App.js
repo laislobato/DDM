4d9c93be2ec7e0c9b4292942f0488231f3f6269f
laislobato <laislobato210109@gmail.com>
2025-03-06 12:26:57 -0300

    0603

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -8,6 +8,15 @@ export default function App() {
         <Text style={styles.titleText}>Silvinha Health App</Text>
       </View>
 
+      <View style={styles.content}>
+        <Text style={styles.subTitle}>Calculadora de IMC</Text>
+
+        <View> 
+          <Text style={styles.label}>Altura</Text>
+        </View>
+
+      </View>
+
       <StatusBar style="light" />
     </SafeAreaView>
   );
@@ -22,7 +31,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-end',
     height: 130,
-    backgroundColor: '#ef233c',
+    backgroundColor: '#7A77FC',
     borderBottomStartRadius: 25,
     borderBottomEndRadius: 25,
   },
@@ -31,5 +40,27 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: 'bold',
     marginBottom: 30,
+  },
+  content: {
+    flex: 1,
+    padding: 40,
+    width: '100%',
+    backgroundColor: '#edf2f'
+  },
+  subTitle: {
+    textAlign: 'center',
+    width: '100%',
+    fontSize: 24,
+    color: '#7A77FC',
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+  label: {
+    textAlign: 'center',
+    width: '100%',
+    fontSize: 20,
+    color: '#7A77FC',
+    fontWeight: '',
+    marginBottom: 40,
   },
 });
